252c06c615be4352cbde1bff7d8b17fd7e488581
alexclou13 <alexclou13@hotmail.com>
2026-04-15 17:39:18 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDTM0t9BY1NsIeAFMGB1uj5cUurAQJsRhE",
   authDomain: "pool-multi-series-2026.firebaseapp.com",
   projectId: "pool-multi-series-2026",
-  storageBucket: "pool-multi-series-2026.appspot.com",
+  storageBucket: "pool-multi-series-2026.firebasestorage.app",
   messagingSenderId: "68600782804",
   appId: "1:68600782804:web:c01d53b9ccd915bf524041"
 };
