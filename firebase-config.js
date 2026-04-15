76b4fd09170705ae7360d1c1062deb9449e8f60c
alexclou13 <alexclou13@hotmail.com>
2026-04-15 17:35:08 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// window.FIREBASE_CONFIG = null;
-//   apiKey: "AIzaSyDTMOt9BY1NSIeAFMGB1uj5cUurAQJsRhE",
-//   authDomain: "pool-multi-series-2026.firebaseapp.com",
-//   projectId: "pool-multi-series-2026",
-//   storageBucket: "pool-multi-series-2026.firebasestorage.app",
-//   messagingSenderId: "68600782804",
-//   appId: "1:68600782804:web:c01d53b9ccd915bf524041"
-// };
-
-window.FIREBASE_CONFIG = null;
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDTM0t9BY1NsIeAFMGB1uj5cUurAQJsRhE",
+  authDomain: "pool-multi-series-2026.firebaseapp.com",
+  projectId: "pool-multi-series-2026",
+  storageBucket: "pool-multi-series-2026.appspot.com",
+  messagingSenderId: "68600782804",
+  appId: "1:68600782804:web:c01d53b9ccd915bf524041"
+};
