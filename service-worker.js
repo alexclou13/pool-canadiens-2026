da038d133f60d2af3f09f5217e3865daf3b8a3cf
alexclou13 <alexclou13@hotmail.com>
2026-04-15 00:34:21 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'pool-hockey-cache-v3';
-const urlsToCache = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).catch(() => {}));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
