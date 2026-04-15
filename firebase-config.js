3db4a85eb3af01fd1a69c3c8e7fc5626f1a77ae0
alexclou13 <alexclou13@hotmail.com>
2026-04-15 17:12:08 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Remplace ce bloc avec la config de TON projet Firebase.
 // Exemple:
-// window.FIREBASE_CONFIG = {
-//   apiKey: "...",
-//   authDomain: "...",
-//   projectId: "...",
-//   storageBucket: "...",
-//   messagingSenderId: "...",
-//   appId: "..."
+// window.FIREBASE_CONFIG = null;
+//   apiKey: "AIzaSyDTMOt9BY1NSIeAFMGB1uj5cUurAQJsRhE",
+//   authDomain: "pool-multi-series-2026.firebaseapp.com",
+//   projectId: "pool-multi-series-2026",
+//   storageBucket: "pool-multi-series-2026.firebasestorage.app",
+//   messagingSenderId: "68600782804",
+//   appId: "1:68600782804:web:c01d53b9ccd915bf524041"
 // };
 
 window.FIREBASE_CONFIG = null;
