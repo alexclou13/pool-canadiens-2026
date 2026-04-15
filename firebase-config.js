3fc676201e92c85f8429ae4eeacaa736fec3c5ef
alexclou13 <alexclou13@hotmail.com>
2026-04-15 17:07:57 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Remplace ce bloc avec la config de TON projet Firebase.
+// Exemple:
+// window.FIREBASE_CONFIG = {
+//   apiKey: "...",
+//   authDomain: "...",
+//   projectId: "...",
+//   storageBucket: "...",
+//   messagingSenderId: "...",
+//   appId: "..."
+// };
+
+window.FIREBASE_CONFIG = null;
